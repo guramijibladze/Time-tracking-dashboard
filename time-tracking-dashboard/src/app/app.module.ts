b7c0eec2676712d1313e3ba62bc97612708118ce
guramijibladze <guramijibladze@gmail.com>
2022-01-10 23:24:53 +0400

    add

---
+++ time-tracking-dashboard/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainpageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
